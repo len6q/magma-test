1bf207d902e2be59c436d5a5747cf88463cad4f4
len6q <101547536+len6q@users.noreply.github.com>
2023-05-06 17:35:58 +0600

    Add DI

---
+++ MagmaAPI/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MagmaAPI.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MagmaAPI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplication(
+            this IServiceCollection services)
+        {
+            services.AddSingleton<DataController>();
+            services.AddSingleton(Serializer.Deserialize());
+            return services;
+        }
+    }
+}
--- MagmaAPI/Startup.cs
+++ MagmaAPI/Startup.cs
@@ -1,4 +1,3 @@
-using MagmaAPI.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -25,8 +24,7 @@ namespace MagmaAPI
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MagmaAPI", Version = "v1" });
             });
 
-            services.AddSingleton<DataController>();
-            services.AddSingleton(Serializer.Deserialize());
+            services.AddAplication();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
